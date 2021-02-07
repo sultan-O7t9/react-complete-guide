75ea55fd04010fdc0862d2607076fbb584b8b05c
sultan-O7t9 <72926764+sultan-O7t9@users.noreply.github.com>
2021-02-07 15:18:02 +0500

    converted app into functional component and used React Hook: useState for the first time

--- src/App.js
+++ src/App.js
@@ -1,37 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Car from "./Car/Car";
 
-class App extends React.Component {
-  state = {
+// class App extends React.Component {
+//   state = {
+//     cars: [
+//       { brand: "BMW", model: "M4" },
+//       { brand: "Mclaren", model: "P1" },
+//       { brand: "Ferrari", model: "LaFerrari" },
+//     ],
+//   };
+//   switchNameHandler = () => {
+//     console.log("Clicked");
+//   };
+
+//   render() {
+//     return (
+//       <div className="App">
+//         <h1>Hello</h1>
+//         <h2>Props for the first time</h2>
+//         <button onClick={this.switchNameHandler}>Switch Name</button>
+//         <Car
+//           brand={this.state.cars[0].brand}
+//           model={this.state.cars[0].model}
+//         />
+//         <Car brand={this.state.cars[1].brand} model={this.state.cars[1].model}>
+//           Better One!
+//         </Car>
+//         <Car brand={this.state.cars[2].brand} model={this.state.cars[2].model}>
+//           Best One!
+//         </Car>
+//       </div>
+//     );
+//   }
+// }
+const App = () => {
+  const [currentCar, setCarState] = useState({
     cars: [
       { brand: "BMW", model: "M4" },
       { brand: "Mclaren", model: "P1" },
       { brand: "Ferrari", model: "LaFerrari" },
     ],
-  };
-  switchNameHandler = () => {
-    console.log("Clicked");
+  });
+
+  const switchNameHandler = () => {
+    setCarState({
+      cars: [
+        { brand: "Lambo", model: "M4" },
+        { brand: "Mclaren", model: "P1" },
+        { brand: "Ferrari", model: "LaFerrari" },
+      ],
+    });
   };
 
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello</h1>
-        <h2>Props for the first time</h2>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Car
-          brand={this.state.cars[0].brand}
-          model={this.state.cars[0].model}
-        />
-        <Car brand={this.state.cars[1].brand} model={this.state.cars[1].model}>
-          Better One!
-        </Car>
-        <Car brand={this.state.cars[2].brand} model={this.state.cars[2].model}>
-          Best One!
-        </Car>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <h2>Props for the first time</h2>
+      <button onClick={switchNameHandler}>Switch Name</button>
+      <Car brand={currentCar.cars[0].brand} model={currentCar.cars[0].model} />
+      <Car brand={currentCar.cars[1].brand} model={currentCar.cars[1].model}>
+        Better One!
+      </Car>
+      <Car brand={currentCar.cars[2].brand} model={currentCar.cars[2].model}>
+        Best One!
+      </Car>
+    </div>
+  );
+};
+
 export default App;
