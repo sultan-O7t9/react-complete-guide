625903128512757a2cbae4f52b492fec3149502a
sultan-O7t9 <72926764+sultan-O7t9@users.noreply.github.com>
2021-02-07 12:41:38 +0500

    Added dynamic content
    Added dynamic content to Car.js using { },

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Car = () => {
-  return <p>I'm a Car</p>;
+  return (
+    <p>
+      I'm a Car, a model built in year {"19" + Math.floor(Math.random() * 30)}
+    </p>
+  );
 };
 
 export default Car;
