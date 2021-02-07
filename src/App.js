6295ad08068fc672e0a7d0f5f559e1a276da9d0a
sultan-O7t9 <72926764+sultan-O7t9@users.noreply.github.com>
2021-02-07 12:23:48 +0500

    Created a Functional Component: Car
    Created a Car component using function syntax

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import React from "react";
 import "./App.css";
+import Car from "./Car/Car";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello Motherfuckers</h1>
+      <Car />
     </div>
   );
 }
---
+++ src/Car/Car.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Car = () => {
+  return <p>I'm a Car</p>;
+};
+
+export default Car;
