9cf5c85605800c4abd330c7b838e4d3cea182025
sultan-O7t9 <72926764+sultan-O7t9@users.noreply.github.com>
2021-02-07 15:50:25 +0500

    A little Complexity is good for health

--- src/App.js
+++ src/App.js
@@ -35,22 +35,32 @@ import Car from "./Car/Car";
 //   }
 // }
 const App = () => {
-  const [currentCar, setCarState] = useState({
+  const oldState = {
     cars: [
       { brand: "BMW", model: "M4" },
       { brand: "Mclaren", model: "P1" },
       { brand: "Ferrari", model: "LaFerrari" },
     ],
-  });
+    change: true,
+  };
+  const newState = {
+    cars: [
+      { ...oldState.cars[0] },
+      { ...oldState.cars[1] },
+      { ...oldState.cars[2] },
+    ],
+  };
+  newState.cars[0].brand = "Lamborghini";
+  newState.change = false;
 
+  const [currentCar, setCarState] = useState(oldState);
   const switchNameHandler = () => {
-    setCarState({
-      cars: [
-        { brand: "Lambo", model: "M4" },
-        { brand: "Mclaren", model: "P1" },
-        { brand: "Ferrari", model: "LaFerrari" },
-      ],
-    });
+    if (currentCar.change) {
+      setCarState(newState);
+    } else {
+      setCarState(oldState);
+    }
+    console.log(currentCar.change);
   };
 
   return (
