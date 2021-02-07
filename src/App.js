4bf5bba65a71c9574e429cfd051fa5cf4f51d146
sultan-O7t9 <72926764+sultan-O7t9@users.noreply.github.com>
2021-02-07 14:13:38 +0500

    state basics
    added a state object.

--- src/App.js
+++ src/App.js
@@ -2,20 +2,32 @@ import React from "react";
 import "./App.css";
 import Car from "./Car/Car";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello Motherfuckers</h1>
-      <h2>Props for the first time</h2>
-      <Car brand="BMW" model="M4" />
-      <Car brand="McLaren" model="P1">
-        Better One!
-      </Car>
-      <Car brand="Ferrari" model="La Ferrari">
-        Best One!
-      </Car>
-    </div>
-  );
-}
+class App extends React.Component {
+  state = {
+    cars: [
+      { brand: "BMW", model: "M4" },
+      { brand: "Mclaren", model: "P1" },
+      { brand: "Ferrari", model: "LaFerrari" },
+    ],
+  };
 
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello</h1>
+        <h2>Props for the first time</h2>
+        <Car
+          brand={this.state.cars[0].brand}
+          model={this.state.cars[0].model}
+        />
+        <Car brand={this.state.cars[1].brand} model={this.state.cars[1].model}>
+          Better One!
+        </Car>
+        <Car brand={this.state.cars[2].brand} model={this.state.cars[2].model}>
+          Best One!
+        </Car>
+      </div>
+    );
+  }
+}
 export default App;
