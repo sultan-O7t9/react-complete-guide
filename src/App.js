81de0a3641779027f7ccbb871079bf12398157d5
sultan-O7t9 <72926764+sultan-O7t9@users.noreply.github.com>
2021-02-08 12:44:20 +0500

    Added two way data binding
    Added two way data binding and passed down the references

--- src/App.js
+++ src/App.js
@@ -2,81 +2,95 @@ import React, { useState } from "react";
 import "./App.css";
 import Car from "./Car/Car";
 
-// class App extends React.Component {
-//   state = {
-//     cars: [
-//       { brand: "BMW", model: "M4" },
-//       { brand: "Mclaren", model: "P1" },
-//       { brand: "Ferrari", model: "LaFerrari" },
-//     ],
-//   };
-//   switchNameHandler = () => {
-//     console.log("Clicked");
-//   };
-
-//   render() {
-//     return (
-//       <div className="App">
-//         <h1>Hello</h1>
-//         <h2>Props for the first time</h2>
-//         <button onClick={this.switchNameHandler}>Switch Name</button>
-//         <Car
-//           brand={this.state.cars[0].brand}
-//           model={this.state.cars[0].model}
-//         />
-//         <Car brand={this.state.cars[1].brand} model={this.state.cars[1].model}>
-//           Better One!
-//         </Car>
-//         <Car brand={this.state.cars[2].brand} model={this.state.cars[2].model}>
-//           Best One!
-//         </Car>
-//       </div>
-//     );
-//   }
-// }
-const App = () => {
-  const oldState = {
+class App extends React.Component {
+  state = {
     cars: [
       { brand: "BMW", model: "M4" },
       { brand: "Mclaren", model: "P1" },
       { brand: "Ferrari", model: "LaFerrari" },
     ],
-    change: true,
   };
-  const newState = {
-    cars: [
-      { ...oldState.cars[0] },
-      { ...oldState.cars[1] },
-      { ...oldState.cars[2] },
-    ],
+  switchNameHandler = () => {
+    console.log("Clicked");
   };
-  newState.cars[0].brand = "Lamborghini";
-  newState.change = false;
-
-  const [currentCar, setCarState] = useState(oldState);
-  const switchNameHandler = () => {
-    if (currentCar.change) {
-      setCarState(newState);
-    } else {
-      setCarState(oldState);
-    }
-    console.log(currentCar.change);
+  changeNameHandler = (event) => {
+    this.setState({
+      cars: [
+        { brand: "BMW", model: "M4" },
+        { brand: "Mclaren", model: "P1" },
+        { brand: event.target.value, model: "LaFerrari" },
+      ],
+    });
   };
 
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-      <h2>Props for the first time</h2>
-      <button onClick={switchNameHandler}>Switch Name</button>
-      <Car brand={currentCar.cars[0].brand} model={currentCar.cars[0].model} />
-      <Car brand={currentCar.cars[1].brand} model={currentCar.cars[1].model}>
-        Better One!
-      </Car>
-      <Car brand={currentCar.cars[2].brand} model={currentCar.cars[2].model}>
-        Best One!
-      </Car>
-    </div>
-  );
-};
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello</h1>
+        <h2>Props for the first time</h2>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Car
+          brand={this.state.cars[0].brand}
+          model={this.state.cars[0].model}
+        />
+        <Car brand={this.state.cars[1].brand} model={this.state.cars[1].model}>
+          Better One!
+        </Car>
+        <Car
+          brand={this.state.cars[2].brand}
+          model={this.state.cars[2].model}
+          change={this.changeNameHandler}
+        >
+          Best One!
+        </Car>
+      </div>
+    );
+  }
+}
+
+// const App = () => {
+//   const oldState = {
+//     cars: [
+//       { brand: "BMW", model: "M4" },
+//       { brand: "Mclaren", model: "P1" },
+//       { brand: "Ferrari", model: "LaFerrari" },
+//     ],
+//     change: true,
+//   };
+//   const newState = {
+//     cars: [
+//       { ...oldState.cars[0] },
+//       { ...oldState.cars[1] },
+//       { ...oldState.cars[2] },
+//     ],
+//   };
+//   newState.cars[0].brand = "Lamborghini";
+//   newState.change = false;
+
+//   const [currentCar, setCarState] = useState(oldState);
+//   const switchNameHandler = () => {
+//     if (currentCar.change) {
+//       setCarState(newState);
+//     } else {
+//       setCarState(oldState);
+//     }
+//     console.log(currentCar.change);
+//   };
+
+//   return (
+//     <div className="App">
+//       <h1>Hello</h1>
+//       <h2>Props for the first time</h2>
+//       <button onClick={switchNameHandler}>Switch Name</button>
+//       <Car brand={currentCar.cars[0].brand} model={currentCar.cars[0].model} />
+//       <Car brand={currentCar.cars[1].brand} model={currentCar.cars[1].model}>
+//         Better One!
+//       </Car>
+//       <Car brand={currentCar.cars[2].brand} model={currentCar.cars[2].model}>
+//         Best One!
+//       </Car>
+//     </div>
+//   );
+// };
 
 export default App;
--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,12 +1,13 @@
 import React from "react";
 
-const Car = ({ brand, model, children }) => {
+const Car = ({ brand, model, children, change }) => {
   return (
     <div>
       <h4>
         I'm a Car built by {brand} and my model is {model}
       </h4>
       <p>{children}</p>
+      <input type="text" placeholder="brand" value={brand} onChange={change} />
     </div>
   );
 };
