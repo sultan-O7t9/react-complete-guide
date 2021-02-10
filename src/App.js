ab654f1430384c01916d16b4d15b3cb9e74875c4
sultan-O7t9 <72926764+sultan-O7t9@users.noreply.github.com>
2021-02-10 16:57:03 +0500

    Working with both class and functional comps-2

--- src/App.js
+++ src/App.js
@@ -144,6 +144,7 @@ const App = () => {
       </div>
     );
   }
+<<<<<<< Updated upstream
   return (
     <div className="App">
       <h1>Hello World</h1>
@@ -154,4 +155,53 @@ const App = () => {
   );
 };
 
+=======
+}
+
+// const App = () => {
+//   const oldState = {
+//     cars: [
+//       { brand: "BMW", model: "M4" },
+//       { brand: "Mclaren", model: "P1" },
+//       { brand: "Ferrari", model: "LaFerrari" },
+//     ],
+//     change: true,
+//   };
+//   const newState = {
+//     cars: [
+//       { ...oldState.cars[0] },
+//       { ...oldState.cars[1] },
+//       { ...oldState.cars[2] },
+//     ],
+//   };
+//   newState.cars[0].brand = "Lamborghini";
+//   newState.change = false;
+
+//   const [currentCar, setCarState] = useState(oldState);
+//   const switchNameHandler = () => {
+//     if (currentCar.change) {
+//       setCarState(newState);
+//     } else {
+//       setCarState(oldState);
+//     }
+//     console.log(currentCar.change);
+//   };
+
+//   return (
+//     <div className="App">
+//       <h1>Hello</h1>
+//       <h2>Props for the first time</h2>
+//       <button onClick={switchNameHandler}>Switch Name</button>
+//       <Car brand={currentCar.cars[0].brand} model={currentCar.cars[0].model} />
+//       <Car brand={currentCar.cars[1].brand} model={currentCar.cars[1].model}>
+//         Better One!
+//       </Car>
+//       <Car brand={currentCar.cars[2].brand} model={currentCar.cars[2].model}>
+//         Best One!
+//       </Car>
+//     </div>
+//   );
+// };
+//.
+>>>>>>> Stashed changes
 export default App;
